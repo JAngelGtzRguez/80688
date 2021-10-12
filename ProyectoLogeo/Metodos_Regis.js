58325e858d371eb2487536026d552422d9651a70
angel.rogu017@gmail.com <angel.rogu017@gmail.com>
2021-10-11 23:43:32 -0500

    Llamar al backend correctamente

--- ProyectoLogeo/Metodos_Regis.js
+++ ProyectoLogeo/Metodos_Regis.js
@@ -1,4 +1,29 @@
-function obtenerdatos(){
+var btnNoSubmitGet = document.getElementById("btnNoSubmitGet");
+btnNoSubmitGet.addEventListener('click', function () {
+    // recupero del formulario los valores de los campos email y password mediante sus IDs
+    let email = document.getElementById('nombre').value;
+    let pass = document.getElementById('password').value;
+    // construyo una URL de parámetros, es decir un quertystring
+    var params = new URLSearchParams();
+    params.append('nombre', email);
+    params.append('password', pass);
+    // console.log(params);
+    // console.log(params.get('nombre'));
+    // console.log(params.get('password'));
+    // invocación de la librería AXIOS
+    axios.get('http://localhost:4567/saludar?'+ params)
+        .then(function (response) {
+            console.log(response);
+            console.log("verdad: " + response.data);
+            alert(response.data)
+        })
+        .catch(function (error) {
+            console.log("error: " + error);
+        })
+});
+
+
+/*function obtenerdatos(){
     var usuario = document.getElementById('usuario').value;
     var passw = document.getElementById('password').value;
 
@@ -16,6 +41,6 @@ function iniciosesion(){
         document.logeo.passobt.value = passw;
     }
 
-}
+}*/
 
 
