aa097727b23903c7f1fe457f233613235cf40267
angel.rogu017@gmail.com <angel.rogu017@gmail.com>
2021-10-13 00:39:01 -0500

    Clase del 12.10.21

---
+++ PROYECTOLOGEO.MAESTRO/src/main/java/mx/uv/App.java
@@ -0,0 +1,69 @@
+package mx.uv;
+
+import static spark.Spark.*;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.HashMap;
+
+import com.google.gson.*;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static Gson gson = new Gson();
+    private static Usuario usuario = new Usuario("1", "angel@gmail.com", "12345"); 
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
+
+        
+
+          
+        get("/usuarios", (req, res)-> {
+            before((rq,rs)-> rs.type("application/json"));
+            return gson.toJson(usuarios.values());
+        });
+
+        post("/usuario", (req, res)-> {
+            String payload = req.body();
+            String id = UUID.randomUUID().toString();
+            Usuario u = gson.fromJson(payload, Usuario.class);
+            u.setId(id);
+            usuarios.put(id, u);
+
+            JsonObject objetoJson = new JsonObject();
+            objetoJson.addProperty("status", "ok");
+            objetoJson.addProperty("id",  id);
+            return objetoJson;
+
+        });
+
+        get("/pagina", (req, res)->{
+            JsonObject objetoJson = new JsonObject();
+            objetoJson.addProperty("access", "ok");
+            objetoJson.addProperty("page",  "http://www.google.com");
+            return objetoJson;
+        });
+        
+    }
+}
